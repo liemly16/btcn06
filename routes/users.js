e3d649cc9bfe58a7c55652cb9d59d223e1ec4066
LL <liembsbs123@gmail.com>
2019-10-12 13:10:20 +0700

    create api register user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
+var User = require("../models/user");
+var passwordHash = require("password-hash");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", function(req, res, next) {
+  let user = new User({
+    username: req.body.username,
+    password: passwordHash.generate(req.body.password)
+  });
+
+  user
+    .save()
+    .then(u => {
+      res.json({
+        result: u
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error" });
+    });
 });
 
 module.exports = router;
